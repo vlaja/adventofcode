6212205484c708e97cc9e02121b5e857ef17b0f8
Vlatko Vlahek <vlatko@prototyp.digital>
2024-12-02 18:06:46 +0100

    Added solutions for part 2

--- Advent Of Code/Day2/Solution.fs
+++ Advent Of Code/Day2/Solution.fs
@@ -7,6 +7,11 @@ let inputFilePath = Path.Combine(__SOURCE_DIRECTORY__, "Input.txt")
 
 let lines = File.ReadLines(inputFilePath)
 
+let processLine (line: string) =
+    line.Split(' ') |> Array.filter (fun part -> part.Trim() <> "") |> Array.map int
+
+let levels = lines |> Seq.map processLine
+
 let isSortedAscending (numbers: int[]) =
     numbers |> Array.pairwise |> Array.forall (fun (a, b) -> a <= b)
 
@@ -20,30 +25,45 @@ let hasValidDifferences (numbers: int[], minOffset: int, maxOffset: int) =
         let difference = abs (b - a)
         difference >= minOffset && difference <= maxOffset)
 
-let isSafe numbers =
+let isSafe (numbers: int[], minOffset: int, maxOffset: int) =
     let isSorted = isSortedAscending numbers || isSortedDescending numbers
-    let hasValidOffsets = hasValidDifferences (numbers, 1, 3)
+    let hasValidOffsets = hasValidDifferences (numbers, minOffset, maxOffset)
     isSorted && hasValidOffsets
 
-let levels =
-    lines
-    |> Seq.map (fun line -> line.Split(' ') |> Array.filter (fun part -> part.Trim() <> "") |> Array.map int)
+let rec generateCombinations (arr: int[]) n =
+    if n = 0 then
+        [ arr ]
+    else
+        [ for i in 0 .. Array.length arr - 1 do
+              let rest = Array.append arr.[.. i - 1] arr.[i + 1 ..]
+              yield! generateCombinations rest (n - 1) ]
+
+let applyDampener (numbers: int[], minOffset: int, maxOffset: int) =
+    let numbersToDampen = 1
+
+    [ for i in 1..numbersToDampen do
+          yield! generateCombinations numbers i ]
+    |> List.exists (fun newArray -> isSafe (newArray, minOffset, maxOffset))
+
+let withDampener isSafe (numbers: int[], minOffset: int, maxOffset: int) =
+    match isSafe (numbers, minOffset, maxOffset) with
+    | true -> true
+    | false -> applyDampener (numbers, minOffset, maxOffset)
+
+let countSafeReportsGeneric safetyCheck =
+    levels
+    |> Seq.map (fun numbers -> numbers, safetyCheck (numbers, 1, 3))
+    |> Seq.filter (fun (numbers, isSafe) -> isSafe = true)
+    |> Seq.toList
+    |> List.length
 
 let countSafeReports () =
-    levels
-    |> Seq.map (fun numbers -> numbers, isSafe numbers)
-    |> Seq.filter (fun (numbers, isSafe) -> isSafe = true)
-    |> Seq.toList
-    |> List.length
+    countSafeReportsGeneric isSafe
     |> fun (count) -> printfn $"Number of safe levels: {count}"
-    
+
 let countSafeReportsWithDampener () =
-    levels
-    |> Seq.map (fun numbers -> numbers, isSafe numbers)
-    |> Seq.filter (fun (numbers, isSafe) -> isSafe = true)
-    |> Seq.toList
-    |> List.length
+    countSafeReportsGeneric (withDampener isSafe)
     |> fun (count) -> printfn $"Number of safe levels with problem dampener enabled: {count}"
 
 countSafeReports ()
-countSafeReportsWithDampener()
+countSafeReportsWithDampener ()
