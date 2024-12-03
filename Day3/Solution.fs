f034b4d45c169f6f06968c856e6e77b883d3e319
Vlatko Vlahek <vlatko@prototyp.digital>
2024-12-03 16:19:55 +0100

    Added Solution for day3/part2

--- Day3/Solution.fs
+++ Day3/Solution.fs
@@ -4,26 +4,54 @@ module Advent_Of_Code.Day3.Solution
 open System.IO
 open System.Text.RegularExpressions
 open FSharp.Collections
+open FSharp.Core
 
 let inputFilePath = Path.Combine(__SOURCE_DIRECTORY__, "Input.txt")
 
 let lines = File.ReadLines(inputFilePath)
+let multiplicationPattern = @"mul\((\d{1,3}),(\d{1,3})\)"
+let stopPattern = @"don't\(\)"
+let resumePattern = @"do\(\)"
 
-let findValidCommands (input: string) =
-    let pattern = @"mul\((\d{1,3}),(\d{1,3})\)"
-    let matches = Regex.Matches(input, pattern)
-
+let multiplyMatches (matches: List<Match>) =
     [ for m in matches ->
-          let x = int m.Groups.[1].Value
-          let y = int m.Groups.[2].Value
+          let x = int m.Groups[1].Value
+          let y = int m.Groups[2].Value
           (x, y) ]
+    |> Seq.map (fun (x, y) -> x * y)
+
+let filterMatchesBetweenDontAndDo (matches: seq<Match>) =
+    matches
+    |> Seq.fold
+        (fun (isSkipping, acc) m ->
+            match m.Value with
+            | v when Regex.IsMatch(v, stopPattern) -> (true, acc)
+            | v when Regex.IsMatch(v, resumePattern) -> (false, acc)
+            | v when Regex.IsMatch(v, multiplicationPattern) && not isSkipping -> (isSkipping, m :: acc)
+            | _ -> (isSkipping, acc))
+        (false, [])
+    |> snd
+
+let findValidCommands input =
+    Regex.Matches(input, multiplicationPattern) |> Seq.toList |> multiplyMatches
+
+let findValidCommandsWithStopAndResume (input: string) =
+    Regex.Matches(input, $"{multiplicationPattern}|{stopPattern}|{resumePattern}")
+    |> filterMatchesBetweenDontAndDo
+    |> multiplyMatches
+
+let summarise finder =
+    lines |> String.concat " " |> finder |> Seq.sum
 
 // Part one
 let findMatches () =
-    lines
-    |> Seq.collect findValidCommands
-    |> Seq.map (fun (x, y) -> x * y)
-    |> Seq.sum
-    |> fun sum -> printfn $"Total score: {sum}"
+    summarise findValidCommands
+    |> fun sum -> printfn $"Total multiplication score: {sum}"
+
+// Part two
+let findEnabledMatches () =
+    summarise findValidCommandsWithStopAndResume
+    |> fun sum -> printfn $"Total multiplication score with for enabled matches: {sum}"
 
 findMatches ()
+findEnabledMatches ()
